fix: use scipy parameter convention and numerically stable formulas for exponnorm

src/numba_stats/exponnorm.py
@@ -1,15 +1,18 @@
-r"""
+"""
 Exponentially modified normal distribution.
 
-The distribution is defined as the distribution of the sum of a normal and
-exponential random variate. This definition has a right tail, but a left
-tailed version can be defined by taking the difference between a normal and
-exponential random variate instead.
-
-To convert between the left- and right-tailed versions, a coordinate
-transformation x -> 2\mu - x can be used to reflect the x-axis.
+The distribution of the sum of a normal and an exponential random variate. It has a
+right tail. The left-tailed version, which is used to model signal loss in detectors,
+for example, in fits of gamma lines, is obtained by reflecting the variate, x -> 2 loc
+- x.
 
 https://en.wikipedia.org/wiki/Exponentially_modified_Gaussian_distribution
+
+Notes
+-----
+The shape parameter K is the mean of the exponential component divided by the standard
+deviation of the normal component, K = tau / scale, which follows the convention of
+scipy.stats.exponnorm.
 
 See Also
 --------
@@ -17,12 +20,10 @@ scipy.stats.exponnorm: Scipy equivalent.
 """
 
 from math import erfc as _erfc
-from math import exp as _exp
-from math import sqrt as _sqrt
 
 import numpy as np
 
-from . import norm as _norm
+from ._special import erfcx as _erfcx
 from ._util import (
     _generate_wrappers,
     _jit,
@@ -34,57 +35,76 @@ from ._util import (
 )
 
 _doc_par = """
+K : float
+    Shape parameter. Mean of the exponential component divided by the standard
+    deviation of the normal component. Must be positive.
 loc : float
-    Mode of unmodified normal distribution.
+    Location of the mode of the normal component.
 scale : float
-    Standard deviation.
-tau : float
-    Exponential decay parameter.
+    Standard deviation of the normal component.
 """
 
 
-@_jit_pointwise(4)
-def _pdf1(x: float, loc: float, scale: float, tau: float) -> float:
-    T = type(x)
-    sqrt2 = T(_sqrt(2))
-    arg1 = (scale**2) / (T(2) * tau**2) - (x - loc) / tau
-    arg2 = (scale) / (sqrt2 * tau) - (x - loc) / (sqrt2 * scale)
-    return T(1) / (T(2) * tau) * _exp(arg1) * _erfc(arg2)
-
-
-@_jit_pointwise(4)
-def _cdf1(x: float, loc: float, scale: float, tau: float) -> float:
-    T = type(x)
-    sqrt2 = T(_sqrt(2))
+@_jit_pointwise(2, cache=False)  # cannot cache because of _erfcx
+def _logpdf1(z: float, K: float) -> float:
+    # The textbook formula exp(v * v / 2 - z * v) * erfc(u) with v = 1 / K and
+    # u = (v - z) / sqrt(2) overflows for small K. We use the identity
+    # v * v / 2 - z * v - u * u = -z * z / 2 to rewrite it in terms of the scaled
+    # complementary error function erfcx(u) = exp(u * u) * erfc(u) for u >= 0.
+    T = type(z)
     half = T(0.5)
-    arg1 = (scale**2) / (T(2) * tau**2) - (x - loc) / tau
+    v = T(1) / K
+    u = (v - z) * T(np.sqrt(0.5))
+    c = -np.log(T(2) * K)
+    if u >= 0:
+        return c - half * z * z + np.log(T(_erfcx(u)))  # type:ignore[no-any-return]
+    # erfc(u) is between 1 and 2 here and the exponent is negative
+    return c + half * v * v - z * v + np.log(T(_erfc(u)))  # type:ignore[no-any-return]
+
+
+@_jit_pointwise(2, cache=False)  # cannot cache because of _erfcx
+def _cdf1(z: float, K: float) -> float:
+    # see _logpdf1 for the rationale of the two branches
+    T = type(z)
+    half = T(0.5)
+    v = T(1) / K
+    u = (v - z) * T(np.sqrt(0.5))
+    if u >= 0:
+        corr = np.exp(-half * z * z) * T(_erfcx(u))
+    else:
+        corr = np.exp(half * v * v - z * v) * T(_erfc(u))
+    # erfc(-z) instead of 1 + erf(z) to retain relative precision in the left tail
+    return half * (T(_erfc(-z * T(np.sqrt(0.5)))) - corr)  # type:ignore[no-any-return]
+
+
+@_jit(3, cache=False)
+def _logpdf(x: np.ndarray, K: float, loc: float, scale: float) -> np.ndarray:
     z = _trans(x, loc, scale)
-    arg2 = (scale) / (sqrt2 * tau) - (z / sqrt2)
-    return _norm._cdf1(z) - half * _exp(arg1) * _erfc(arg2)
+    c = np.log(scale)
+    for i in _prange(len(z)):
+        z[i] = _logpdf1(z[i], K) - c
+    return z
 
 
-@_jit(3)
-def _pdf(x: np.ndarray, loc: float, scale: float, tau: float) -> np.ndarray:
-    r = np.empty_like(x)
-    for i in _prange(len(x)):
-        r[i] = _pdf1(x[i], loc, scale, tau)
-    return r
+@_jit(3, cache=False)
+def _pdf(x: np.ndarray, K: float, loc: float, scale: float) -> np.ndarray:
+    return np.exp(_logpdf(x, K, loc, scale))
 
 
-@_jit(3)
-def _cdf(x: np.ndarray, loc: float, scale: float, tau: float) -> np.ndarray:
-    r = np.empty_like(x)
-    for i in _prange(len(x)):
-        r[i] = _cdf1(x[i], loc, scale, tau)
-    return r
+@_jit(3, cache=False)
+def _cdf(x: np.ndarray, K: float, loc: float, scale: float) -> np.ndarray:
+    z = _trans(x, loc, scale)
+    for i in _prange(len(z)):
+        z[i] = _cdf1(z[i], K)
+    return z
 
 
 @_rvs_jit(3)
 def _rvs(
-    loc: float, scale: float, tau: float, size: int, random_state: int | None
+    K: float, loc: float, scale: float, size: int, random_state: int | None
 ) -> np.ndarray:
     _seed(random_state)
-    return np.random.normal(loc, scale, size) + np.random.exponential(tau, size)
+    return np.random.normal(loc, scale, size) + np.random.exponential(K * scale, size)
 
 
 _generate_wrappers(globals())

src/numba_stats/truncexponnorm.py
@@ -1,19 +1,13 @@
-r"""
+"""
 Truncated exponentially modified normal distribution.
 
-The distribution is defined as the distribution of the sum of a normal and
-exponential random variate. This definition has a right tail, but a left
-tailed version can be defined by taking the difference between a normal and
-exponential random variate instead.
-
-To convert between the left- and right-tailed versions, a coordinate
-transformation x -> 2\mu - x can be used to reflect the x-axis.
-
-https://en.wikipedia.org/wiki/Exponentially_modified_Gaussian_distribution
+See the module exponnorm for a description of the untruncated distribution. There is
+no scipy equivalent. The truncation parameters xmin and xmax follow the convention of
+truncnorm and truncexpon.
 
 See Also
 --------
-scipy.stats.exponnorm: Untruncated scipy equivalent.
+scipy.stats.exponnorm: Scipy equivalent of the untruncated distribution.
 """
 
 import numpy as np
@@ -26,48 +20,62 @@ xmin : float
     Lower edge of the distribution.
 xmax : float
     Upper edge of the distribution.
+K : float
+    Shape parameter. Mean of the exponential component divided by the standard
+    deviation of the normal component. Must be positive.
 loc : float
-    Location parameter.
+    Location of the mode of the normal component.
 scale : float
-    Scale parameter.
-tau : float
-    Exponential decay parameter.
+    Standard deviation of the normal component.
 """
 
 
-@_jit(5)
+@_jit(5, cache=False)
+def _logpdf(
+    x: np.ndarray, xmin: float, xmax: float, K: float, loc: float, scale: float
+) -> np.ndarray:
+    T = type(scale)
+    scale2 = T(1) / scale
+    z = (x - loc) * scale2
+    zmin = (xmin - loc) * scale2
+    zmax = (xmax - loc) * scale2
+    c = np.log(scale * (_exponnorm._cdf1(zmax, K) - _exponnorm._cdf1(zmin, K)))
+    for i in _prange(len(z)):
+        if zmin <= z[i] < zmax:
+            z[i] = _exponnorm._logpdf1(z[i], K) - c
+        else:
+            z[i] = -T(np.inf)
+    return z
+
+
+@_jit(5, cache=False)
 def _pdf(
-    x: np.ndarray, xmin: float, xmax: float, loc: float, scale: float, tau: float
+    x: np.ndarray, xmin: float, xmax: float, K: float, loc: float, scale: float
 ) -> np.ndarray:
-    T = type(scale)
-    pmin = _exponnorm._cdf1(xmin, loc, scale, tau)
-    pmax = _exponnorm._cdf1(xmax, loc, scale, tau)
-    r = np.zeros_like(x)
-    for i in _prange(len(x)):
-        if xmin <= x[i] < xmax:
-            r[i] = _exponnorm._pdf1(x[i], loc, scale, tau) / (pmax - pmin)
-        else:
-            r[i] = T(0.0)
-    return r
+    return np.exp(_logpdf(x, xmin, xmax, K, loc, scale))
 
 
-@_jit(5)
+@_jit(5, cache=False)
 def _cdf(
-    x: np.ndarray, xmin: float, xmax: float, loc: float, scale: float, tau: float
+    x: np.ndarray, xmin: float, xmax: float, K: float, loc: float, scale: float
 ) -> np.ndarray:
     T = type(scale)
-    pmin = _exponnorm._cdf1(xmin, loc, scale, tau)
-    pmax = _exponnorm._cdf1(xmax, loc, scale, tau)
-    r = np.zeros_like(x)
-    for i in _prange(len(x)):
-        if xmin <= x[i]:
-            if x[i] < xmax:
-                r[i] = (_exponnorm._cdf1(x[i], loc, scale, tau) - pmin) / (pmax - pmin)
+    scale2 = T(1) / scale
+    z = (x - loc) * scale2
+    zmin = (xmin - loc) * scale2
+    zmax = (xmax - loc) * scale2
+    pmin = _exponnorm._cdf1(zmin, K)
+    pmax = _exponnorm._cdf1(zmax, K)
+    scale3 = T(1) / (pmax - pmin)
+    for i in _prange(len(z)):
+        if zmin <= z[i]:
+            if z[i] < zmax:
+                z[i] = (_exponnorm._cdf1(z[i], K) - pmin) * scale3
             else:
-                r[i] = T(1.0)
+                z[i] = T(1)
         else:
-            r[i] = T(0.0)
-    return r
+            z[i] = T(0)
+    return z
 
 
 _generate_wrappers(globals())

tests/test_truncexponnorm.py
@@ -1,59 +1,87 @@
+import numba as nb
 import numpy as np
 import pytest
 import scipy.stats as sc
+from numpy.testing import assert_allclose
 from scipy.integrate import quad
 
 from numba_stats import truncexponnorm
 
 
+@pytest.mark.parametrize("K", (0.03, 0.5, 3.0))
 @pytest.mark.parametrize("mu", (0, -1, 1))
 @pytest.mark.parametrize("sigma", (1, 0.5, 2))
-@pytest.mark.parametrize("tau", (1, 2, 3))
-def test_truncnorm(mu, sigma, tau):
-    got = quad(lambda x: truncexponnorm.pdf(x, -1, 1, mu, sigma, tau), -10, 10)[0]
-    expected = 1.0
-    np.testing.assert_allclose(got, expected)
+def test_pdf_is_normalized(K, mu, sigma):
+    got = quad(lambda x: truncexponnorm.pdf(x, -1, 1, K, mu, sigma), -1, 1)[0]
+    assert_allclose(got, 1)
 
 
-def test_pdf():
-    x = np.linspace(-1, 5, 10)
-    xmin = 1
-    xmax = 4
-    mu = 2
-    sigma = 3
-    tau = 1
-    K = tau / sigma
-    got = truncexponnorm.pdf(x, xmin, xmax, mu, sigma, tau)
-    # We have to manually truncate the distribution since scipy doesn't provide one
-    pmin = sc.exponnorm.cdf(xmin, K, loc=mu, scale=sigma)
-    pmax = sc.exponnorm.cdf(xmax, K, loc=mu, scale=sigma)
-    expected = np.where(
+def scipy_pdf(x, xmin, xmax, K, mu, sigma):
+    # scipy has no truncated version, we truncate manually
+    pmin = sc.exponnorm.cdf(xmin, K, mu, sigma)
+    pmax = sc.exponnorm.cdf(xmax, K, mu, sigma)
+    return np.where(
         (xmin <= x) & (x < xmax),
-        sc.exponnorm.pdf(x, K, loc=mu, scale=sigma) / (pmax - pmin),
+        sc.exponnorm.pdf(x, K, mu, sigma) / (pmax - pmin),
         0.0,
     )
-    np.testing.assert_allclose(got, expected)
 
 
-def test_cdf():
+def scipy_cdf(x, xmin, xmax, K, mu, sigma):
+    pmin = sc.exponnorm.cdf(xmin, K, mu, sigma)
+    pmax = sc.exponnorm.cdf(xmax, K, mu, sigma)
+    return np.clip((sc.exponnorm.cdf(x, K, mu, sigma) - pmin) / (pmax - pmin), 0, 1)
+
+
+@pytest.mark.parametrize("K", (0.03, 0.5, 3.0))
+def test_pdf(K):
     x = np.linspace(-1, 5, 10)
-    xmin = 1
-    xmax = 4
-    mu = 2
-    sigma = 3
-    tau = 1
-    K = tau / sigma
-    got = truncexponnorm.cdf(x, xmin, xmax, mu, sigma, tau)
-    # We have to manually truncate the distribution since scipy doesn't provide one
-    pmin = sc.exponnorm.cdf(xmin, K, loc=mu, scale=sigma)
-    pmax = sc.exponnorm.cdf(xmax, K, loc=mu, scale=sigma)
-    expected = np.where(
-        x < xmin,
-        0.0,
-        np.where(
-            x > xmax,
-            1.0,
-            (sc.exponnorm.cdf(x, K, loc=mu, scale=sigma) - pmin) / (pmax - pmin),
-        ),
-    )
-    np.testing.assert_allclose(got, expected)
+    par = 1, 4, K, 2, 3
+    got = truncexponnorm.pdf(x, *par)
+    expected = scipy_pdf(x, *par)
+    assert_allclose(got, expected)
+
+
+@pytest.mark.parametrize("K", (0.03, 0.5, 3.0))
+def test_logpdf(K):
+    x = np.linspace(-1, 5, 10)
+    par = 1, 4, K, 2, 3
+    got = truncexponnorm.logpdf(x, *par)
+    with np.errstate(divide="ignore"):
+        expected = np.log(scipy_pdf(x, *par))
+    assert_allclose(got, expected)
+
+
+@pytest.mark.parametrize("K", (0.03, 0.5, 3.0))
+def test_cdf(K):
+    x = np.linspace(-1, 5, 10)
+    par = 1, 4, K, 2, 3
+    got = truncexponnorm.cdf(x, *par)
+    expected = scipy_cdf(x, *par)
+    assert_allclose(got, expected)
+
+
+def test_support():
+    par = 1, 4, 0.5, 2, 3
+    assert truncexponnorm.pdf(1, *par) > 0
+    assert truncexponnorm.pdf(4, *par) == 0
+    assert truncexponnorm.logpdf(0, *par) == -np.inf
+    assert truncexponnorm.cdf(0, *par) == 0
+    assert truncexponnorm.cdf(1, *par) == 0
+    assert truncexponnorm.cdf(4, *par) == 1
+
+
+@pytest.mark.filterwarnings("error")
+@pytest.mark.parametrize(
+    "fn", [truncexponnorm.logpdf, truncexponnorm.pdf, truncexponnorm.cdf]
+)
+@pytest.mark.parametrize("parallel", [False, True])
+def test_njit(fn, parallel):
+    @nb.njit(parallel=parallel, fastmath=True)
+    def test(x):
+        return fn(x, -1.0, 4.0, 0.5, 1.0, 2.0)
+
+    x = np.linspace(-3, 6, 1000)
+    y = test(x)
+
+    assert_allclose(y, fn(x, -1, 4, 0.5, 1, 2))